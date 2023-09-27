ecf3591fe327820d2e83ab36a7563ad6ad28bbe9
David Aguilar <davvid@gmail.com>
2023-09-26 23:35:18 -0700

    docs: add intersphinx mappings and nitpicky config
    Use the same configuration as rst.linker.

--- docs/conf.py
+++ docs/conf.py
@@ -26,3 +26,16 @@ link_files = {
         ],
     )
 }
+
+# Be strict about any broken references
+nitpicky = True
+
+# Include Python intersphinx mapping to prevent failures
+# jaraco/skeleton#51
+extensions += ['sphinx.ext.intersphinx']
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
+
+# Preserve authored syntax for defaults
+autodoc_preserve_defaults = True
